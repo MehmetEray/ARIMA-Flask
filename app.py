ac191f60f934fbb8d845c378688fcb97f58fe149
Mehmet Eray Sürmeli <39830419+MehmetEray@users.noreply.github.com>
2020-05-11 17:44:54 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,26 +1,25 @@
-import datetime
 import itertools
-import pickle
+import os
 import warnings
-import plotly.graph_objects as go
-import matplotlib
-import matplotlib.pyplot as plt
-import pandas as pd
-import statsmodels.api as sm
-from dateutil.relativedelta import relativedelta
-from flask import Flask, flash, request, redirect, render_template
-from datetime import datetime
 
+import matplotlib.pyplot as plt
+import mpld3
+import numpy as np
+import pandas as pd
+import pdfkit as pdfkit
+import pmdarima as pm
+import statsmodels.api as sm
+from flask import Flask, flash, request, redirect, render_template, make_response
+from flask_weasyprint import HTML
+
+PEOPLE_FOLDER = os.path.join('static')
 ALLOWED_EXTENSIONS = {'excel', 'csv', 'txt'}
 
 warnings.filterwarnings('ignore')
 
 app = Flask(__name__)
 
-
-# logging_conf_path = os.path.normpath(os.path.join(os.path.dirname(__file__), 'logging.conf'))
-# logging.config.fileConfig(logging_conf_path)
-# log = logging.getLogger(__name__)
+app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
 
 
 def allowed_file(filename):
@@ -28,62 +27,7 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-# def create_model(df, traindata):
-#     df.dropna(axis=1, how='all', inplace=True)
-#     """
-#
-#     TEST DATASI DA KULLANILACAK
-#
-#     """
-#
-#
-#     sample = traindata.resample('D').mean()
-#
-#     p = d = q = range(0, 2)
-#     pdq = list(itertools.product(p, d, q))
-#     seasonal_pdq = [(x[0], x[1], x[2], 754) for x in list(itertools.product(p, d, q))]
-#     print('Examples of parameter combinations for Seasonal ARIMA...')
-#     print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
-#     print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
-#     print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
-#     print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
-#
-#     for pr in pdq:
-#         for pr_s in seasonal_pdq:
-#             try:
-#                 mod = sm.tsa.statespace.SARIMAX(y,
-#                                                 order=pr,
-#                                                 seasonal_order=pr_s,
-#                                                 enforce_stationarity=False,
-#                                                 enforce_invertibility=False)
-#                 results = mod.fit()
-#                 print('ARIMA{}x{}12 - AIC:{}'.format(pr, pr_s, results.aic))
-#             except:
-#                 continue
-#
-#     mod = sm.tsa.statespace.SARIMAX(sample,
-#                                     order=(1, 1, 1),
-#                                     seasonal_order=(1, 1, 0, 12),
-#                                     enforce_stationarity=False,
-#                                     enforce_invertibility=False)
-#     results = mod.fit()
-#     pred = results.get_prediction(start=pd.to_datetime('{}'.format(start_date)),
-#                                   end=pd.to_datetime('{}'.format(end_date)), dynamic=False)
-#     conf_date = pred.conf_int().index
-#     conf_lower = pred.conf_int()['lower size']
-#     conf_upper = pred.conf_int()['upper size']
-#     conf_mean = (conf_upper + conf_lower) / 2
-#     results_df = pd.DataFrame({
-#         "conf_date": conf_date,
-#         "conf_lower": conf_lower,
-#         "conf_upper": conf_upper,
-#         "conf_mean": conf_mean
-#     })
-#     print(results_df)
-
-
 def create_pickle(df):
-
     traindata = df
     traindata = traindata.set_index('Tarih')
     traindata.index = pd.to_datetime(traindata.index, unit='ns')
@@ -126,21 +70,21 @@ def create_pickle(df):
     # dt_object1 = datetime.strptime(dt_string, '%Y/%m/%d %H:%M:%S.%f')
     # dt_object2 = datetime.strptime(dt_string2, '%Y/%m/%d %H:%M:%S.%f')
     # pred = results.get_prediction(start=50, end=55)
-    pred = results.get_prediction(start=pd.to_datetime('2017/02/05'),end=pd.to_datetime('2017/07/09'), dynamic=False)
+    pred = results.get_prediction(start=pd.to_datetime('2017/02/05'), end=pd.to_datetime('2017/07/09'), dynamic=False)
 
     pred_ci = pred.conf_int()
     print('ilk')
     print(pred_ci)
-    ax = traindata['2017-01':].plot(label='Öngörülen Veriler')
-    pred.predicted_mean.plot(ax=ax, label='Tahminlenen kısım', alpha=.7, figsize=(12, 4))
-    ax.fill_between(pred_ci.index,
-                    pred_ci.iloc[:, 0],
-                    pred_ci.iloc[:, 1], color='k', alpha=.2)
-    ax.set_xlabel('Tahmini Tarihler')
-    ax.set_ylabel('Disk Boyutu')
-    plt.title('\nTahmin Sonuçları\n')
-    plt.legend()
-    plt.show()
+    # ax = traindata['2017-01':].plot(label='Öngörülen Veriler')
+    # pred.predicted_mean.plot(ax=ax, label='Tahminlenen kısım', alpha=.7, figsize=(12, 4))
+    # ax.fill_between(pred_ci.index,
+    #                 pred_ci.iloc[:, 0],
+    #                 pred_ci.iloc[:, 1], color='k', alpha=.2)
+    # ax.set_xlabel('Tahmini Tarihler')
+    # ax.set_ylabel('boyut')
+    # plt.title('\nTahmin Sonuçları\n')
+    # plt.legend()
+    # plt.show()
     # Get forecast 500 steps ahead in future
     pred_uc = results.get_forecast(steps=500)
 
@@ -155,7 +99,7 @@ def create_pickle(df):
                     pred_ci.iloc[:, 0],
                     pred_ci.iloc[:, 1], color='k', alpha=.2)
     ax.set_xlabel('Tahmini Tarihler')
-    ax.set_ylabel('Disk ')
+    ax.set_ylabel('boyut ')
     plt.title('\nTahmin Sonuçları\n')
     plt.legend()
     plt.show()
@@ -190,7 +134,6 @@ def create_pickle(df):
     results_df.reset_index(drop=True, inplace=True)
     results_df.set_index('conf_date')
 
-
     dictionary = results_df.to_dict(orient='records')
 
     for key in dictionary:
@@ -200,78 +143,6 @@ def create_pickle(df):
     return dictionary
 
 
-# def predict_model_(dataframe, content, results):
-#     start_date = content['start_date']
-#     end_date = content['end_date']
-#     predicted_df = pd.DataFrame(dataframe)
-#     predicted_df['date'] = pd.to_datetime(predicted_df['date'])
-#
-#     pred = results.get_prediction(start=pd.to_datetime('{}'.format(start_date)),
-#                                   end=pd.to_datetime('{}'.format(end_date)), dynamic=False)
-#     conf_date = pred.conf_int().index
-#     conf_lower = pred.conf_int()['lower size']
-#     conf_upper = pred.conf_int()['upper size']
-#     conf_mean = (conf_upper + conf_lower) / 2
-#
-#     results_df = pd.DataFrame({
-#         "conf_date": conf_date,
-#         "conf_lower": conf_lower,
-#         "conf_upper": conf_upper,
-#         "conf_mean": conf_mean
-#     })
-#     dictionary = {
-#         'date': [],
-#         'lower': [],
-#         'upper': [],
-#         'mean': []
-#     }
-#     results_df.reset_index(drop=True, inplace=True)
-#     results_df.set_index('conf_date')
-#
-#     dictionary = results_df.to_dict(orient='records')
-#     for key in dictionary:
-#         date_time = key['conf_date'].strftime("%m/%d/%Y, %H:%M:%S")
-#         key['conf_date'] = date_time
-#     dictionary2 = str(dictionary)
-#     print(dictionary2)
-
-# def jupyter(df):
-#     df.dropna(axis=1, how='all', inplace=True)
-#
-#     size = df.copy()
-#     train = size.sample(frac=0.7)
-#     test = size.drop(train.index)
-#     train = train.set_index('Tarih')
-#     sample = train.resample('D').mean()
-#     p = d = q = range(0, 2)
-#     pdq = list(itertools.product(p, d, q))
-#     seasonal_pdq = [(x[0], x[1], x[2], 754) for x in list(itertools.product(p, d, q))]
-#     print('Examples of parameter combinations for Seasonal ARIMA...')
-#     print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
-#     print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
-#     print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
-#     print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
-#     for pr in pdq:
-#         for pr_s in seasonal_pdq:
-#             try:
-#                 mod = sm.tsa.statespace.SARIMAX(y,
-#                                                 order=pr,
-#                                                 seasonal_order=pr_s,
-#                                                 enforce_stationarity=False,
-#                                                 enforce_invertibility=False)
-#                 results = mod.fit()
-#                 print('ARIMA{}x{}12 - AIC:{}'.format(pr, pr_s, results.aic))
-#             except:
-#                 continue
-#     mod = sm.tsa.statespace.SARIMAX(sample,
-#                                     order=(1, 1, 1),
-#                                     seasonal_order=(1, 1, 0, 12),
-#                                     enforce_stationarity=False,
-#                                     enforce_invertibility=False)
-#     results = mod.fit()
-#     pred = results.get_prediction(start=pd.to_datetime('2018-01-01'), end=pd.to_datetime('2020-04-05'), dynamic=False)
-#     pred_ci = pred.conf_int()
-#     print(pred_ci)
 
 
 @app.route('/')
@@ -294,7 +165,7 @@ def upload_file():
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            dataframe = pd.read_csv(request.files['file'])
+            dataframe = pd.read_csv(request.files['file'], header=0, index_col=0)
             file = request.files['file']
             filename = file.filename
             print('{} isimli dosya yuklendi'.format(filename))
@@ -302,26 +173,148 @@ def upload_file():
             if 'submit-button' in request.form:
                 user_answer = request.form['month']
                 if user_answer == 'one':
-                    lastdate = dataframe["Tarih"].iloc[-1]
-                    # content = {
-                    #     "start_date": "2020-05-07 00:00:00.0",
-                    #     "end_date": "2020-05-07 00:00:00.0"
-                    # }
+                    data = dataframe
+                    model = pm.auto_arima(data.values, start_p=1, start_q=1,
+                                          test='adf',  # use adftest to find optimal 'd'
+                                          max_p=5, max_q=3,  # maximum p and q
+                                          m=1,  # frequency of series
+                                          d=None,  # let model determine 'd'
+                                          seasonal=False,  # No Seasonality
+                                          start_P=0,
+                                          D=0,
+                                          trace=True,
+                                          error_action='ignore',
+                                          suppress_warnings=True,
+                                          stepwise=True)
 
-                    dict = create_pickle(dataframe)
-                    return dict
+                    print(model.summary())
+                    n_periods = 30
+                    fc, confint = model.predict(n_periods=n_periods, return_conf_int=True)
+                    index_of_fc = np.arange(len(data.values), len(data.values) + n_periods)
+
+                    # make series for plotting purposer
+                    fc_series = pd.Series(fc, index=index_of_fc)
+                    lower_series = pd.Series(confint[:, 0], index=index_of_fc)
+                    upper_series = pd.Series(confint[:, 1], index=index_of_fc)
+
+
+                    # Plotr
+                    plt.figure(figsize=(12, 5), dpi=100)
+                    plt.plot(data.values, label='Gerçek Veri')
+                    plt.plot(fc_series, color='red', label="Tahmin")
+                    plt.title("Gelecek 1 Ay Tahminlemesi", size=20)
+                    plt.xlabel("Günler", size=15)
+                    plt.ylabel("Inbound", size=15)
+                    plt.legend(loc='upper right', fontsize=11)
+                    plt.savefig('static/biraylik.png')
+                    full_filename = os.path.join('static/biraylik.png')
+                    return render_template('index.html', result=fc_series, user_image=full_filename)
+
                 if user_answer == 'three':
-                    dataframe['Tarih'] = pd.to_datetime(dataframe['Tarih'])
-                    lastdate = dataframe["Tarih"].iloc[-1]
-                    use_date = lastdate + relativedelta(months=+1)
-                    print(dataframe.dtypes)
-                    print(dataframe)
-                    return use_date
-                if user_answer == 'six':
-                    lastdate = dataframe["Tarih"].iloc[-1]
-                    return lastdate
+                    data = dataframe
+                    model = pm.auto_arima(data.values, start_p=1, start_q=1,
+                                          test='adf',  # use adftest to find optimal 'd'
+                                          max_p=5, max_q=3,  # maximum p and q
+                                          m=1,  # frequency of series
+                                          d=None,  # let model determine 'd'
+                                          seasonal=False,  # No Seasonality
+                                          start_P=0,
+                                          D=0,
+                                          trace=True,
+                                          error_action='ignore',
+                                          suppress_warnings=True,
+                                          stepwise=True)
 
-        # predict_model_(dataframe,)
+                    print(model.summary())
+                    n_periods = 90
+                    fc, confint = model.predict(n_periods=n_periods, return_conf_int=True)
+                    index_of_fc = np.arange(len(data.values), len(data.values) + n_periods)
+
+                    # make series for plotting purposer
+                    fc_series = pd.Series(fc, index=index_of_fc)
+                    lower_series = pd.Series(confint[:, 0], index=index_of_fc)
+                    upper_series = pd.Series(confint[:, 1], index=index_of_fc)
+
+
+                    # Plotr
+                    plt.figure(figsize=(12, 5), dpi=100)
+                    plt.plot(data.values, label='Gerçek Veri')
+                    plt.plot(fc_series, color='red', label="Tahmin")
+                    plt.title("Gelecek 3 Ay Tahminlemesi", size=20)
+                    plt.xlabel("Günler", size=15)
+                    plt.ylabel("Inbound", size=15)
+                    plt.legend(loc='upper right', fontsize=11)
+                    plt.savefig('static/ucaylik.png')
+                    full_filename = os.path.join('static/ucaylik.png')
+                    return render_template('index.html', result=fc_series, user_image=full_filename)
+
+                if user_answer == 'six':
+                    data = dataframe
+                    model = pm.auto_arima(data.values, start_p=1, start_q=1,
+                                          test='adf',  # use adftest to find optimal 'd'
+                                          max_p=5, max_q=3,  # maximum p and q
+                                          m=1,  # frequency of series
+                                          d=None,  # let model determine 'd'
+                                          seasonal=False,  # No Seasonality
+                                          start_P=0,
+                                          D=0,
+                                          trace=True,
+                                          error_action='ignore',
+                                          suppress_warnings=True,
+                                          stepwise=True)
+
+                    print(model.summary())
+
+
+
+                    n_periods = 180
+                    fc, confint = model.predict(n_periods=n_periods, return_conf_int=True)
+                    index_of_fc = np.arange(len(data.values), len(data.values) + n_periods)
+
+                    # make series for plotting purposer
+                    fc_series = pd.Series(fc, index=index_of_fc)
+                    lower_series = pd.Series(confint[:, 0], index=index_of_fc)
+                    upper_series = pd.Series(confint[:, 1], index=index_of_fc)
+
+
+                    # Plotr
+                    plt.figure(figsize=(12, 5), dpi=100)
+                    plt.plot(data.values, label='Gerçek Veri')
+                    plt.plot(fc_series, color='red', label="Tahmin")
+                    plt.title("Gelecek 6 Ay Tahminlemesi", size=20)
+                    plt.xlabel("Günler", size=15)
+                    plt.ylabel("Inbound", size=15)
+                    plt.legend(loc='upper right', fontsize=11)
+
+
+
+                    plt.savefig('static/altiaylik.png')
+                    full_filename = os.path.join('static/altiaylik.png')
+                    return render_template('index.html', result=fc_series, user_image=full_filename)
+
+
+                    # FOR PDF
+                    # rendered = render_template('index.html', result=fc_series, user_image=full_filename)
+                    # pdf = pdfkit.from_string(rendered,False)
+                    # response = make_response(pdf)
+                    # response.headers['Content-Type'] = 'application/pdf'
+                    # response.headers['Content-Disposition'] = 'attachment; filename=output.pdf'
+                    # return response
+
+            if 'delete-button' in request.form:
+                if os.path.exists("static/biraylik.png"):
+                    os.remove("static/biraylik.png")
+                else:
+                    print("The file does not exist")
+                if os.path.exists("static/ucaylik.png"):
+                    os.remove("static/ucaylik.png")
+                else:
+                    print("The file does not exist")
+                if os.path.exists("static/altiaylik.png"):
+                    os.remove("static/altiaylik.png")
+                else:
+                    print("The file does not exist")
+                return render_template('initial')
 
 
 def main():
